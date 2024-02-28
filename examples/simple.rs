90259c0f4fd5deb5664a34ed608ce77c392299cd
2bonahill <tobias.ambuehl@gmail.com>
2024-02-28 06:24:08 +0100

    db merge

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)]
 use anyhow::Result;
-use nanodb::{nanodb::NanoDB, tree::Tree};
+use nanodb::nanodb::NanoDB;
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 
@@ -13,28 +13,28 @@ async fn main() -> Result<()> {
     let age: i64 = db.get("age")?.into()?;
     let city: String = db.get("address")?.get("city")?.into()?;
     let fruits_value_tree: String = db.get("fruits")?.at(1)?.into()?;
+    let address: Map<String, Value> = db.get("address")?.into()?;
 
     // Setting
     db.insert("age", 40)?;
     db.insert("email", "johndoe@gmail.com")?;
-    db.insert("fruits", vec!["apple", "banana", "orange", "avocado"])?;
+    db.insert("fruits", vec!["apple", "banana", "orange"])?;
     db.insert("hobbies", vec!["ski", "tennis", "fitness", "climbing"])?;
+    db.write()?;
 
     // Tree methods
     let number_of_fruits = db.get("fruits")?.array_len()?;
     let fruits = db.get("fruits")?.array_push("mango")?;
-    dbg!(fruits);
+    let numbers = db
+        .get("numbers")?
+        .array_for_each(|v| {
+            *v = Value::from(v.as_i64().unwrap() * 2i64);
+        })
+        .unwrap();
 
-    let fruits_value_tree: Tree = db.get("fruits")?.at(1)?;
-    let address: Map<String, Value> = db.get("address")?.into()?;
-
-    // value trees
-    let mut numbers: Tree = db.get("numbers")?;
-
-    let numbers = numbers.array_for_each(|v| {
-        *v = Value::from(v.as_i64().unwrap() * 2i64);
-    });
-
+    // Merge
+    let fruits = db.get("fruits")?.array_push("coconut")?;
+    db.merge(fruits)?;
     db.write()?;
 
     Ok(())
--- src/nanodb.rs
+++ src/nanodb.rs
@@ -108,6 +108,89 @@ impl NanoDB {
         Ok(())
     }
 
+    /// Merges a Tree into the JSON data of the NanoDB instance at a given path.
+    ///
+    /// # Arguments
+    ///
+    /// * `tree` - The Tree to merge into the JSON data.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(())` - If the operation was successful.
+    /// * `Err(NanoDBError::RwLockWriteError)` - If there was an error acquiring the write lock.
+    /// * `Err(NanoDBError::InvalidJSONPath)` - If the path does not exist in the JSON data or if a path step is not valid for the current value (e.g., using a key on an array or an index on an object).
+    /// * `Err(NanoDBError::IndexOutOfBounds)` - If an index path step is out of bounds of the array.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut db = NanoDB::new("path/to/json/file.json").unwrap();
+    /// let tree = Tree::new(serde_json::json!({"new_key": "new_value"}), vec![]);
+    /// db.merge(tree, vec![PathStep::Key("key".to_string())]).unwrap();
+    /// assert_eq!(db.get("key").unwrap().inner, serde_json::json!({"new_key": "new_value"}));
+    /// ```
+    pub fn merge(&mut self, tree: Tree) -> Result<(), NanoDBError> {
+        let path = tree.path();
+        let mut data = self._write_lock()?;
+
+        let mut current = &mut *data;
+        for p in path {
+            match p {
+                PathStep::Key(key) => {
+                    if current.is_object() {
+                        let obj = current.as_object_mut().unwrap();
+                        // Check if the key exists, and if so, get a mutable reference to it.
+                        // Otherwise, return an error.
+                        match obj.get_mut(&key) {
+                            Some(value) => current = value,
+                            None => return Err(NanoDBError::InvalidJSONPath),
+                        }
+                    } else {
+                        return Err(NanoDBError::InvalidJSONPath);
+                    }
+                }
+                PathStep::Index(idx) => {
+                    if current.is_array() {
+                        let arr = current.as_array_mut().unwrap();
+                        current = arr.get_mut(idx).ok_or(NanoDBError::IndexOutOfBounds)?;
+                    } else {
+                        return Err(NanoDBError::InvalidJSONPath);
+                    }
+                }
+            }
+        }
+
+        *current = tree.inner;
+        Ok(())
+    }
+
+    /// Merges a Tree into the JSON data of the NanoDB instance and writes the data to the file.
+    ///
+    /// # Arguments
+    ///
+    /// * `tree` - The Tree to merge into the JSON data.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(())` - If the operation was successful.
+    /// * `Err(NanoDBError::RwLockWriteError)` - If there was an error acquiring the write lock.
+    /// * `Err(NanoDBError::InvalidJSONPath)` - If the path does not exist in the JSON data or if a path step is not valid for the current value (e.g., using a key on an array or an index on an object).
+    /// * `Err(NanoDBError::IndexOutOfBounds)` - If an index path step is out of bounds of the array.
+    /// * `Err(NanoDBError::FileWriteError)` - If there was an error writing the data to the file.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut db = NanoDB::new("path/to/json/file.json").unwrap();
+    /// let tree = Tree::new(serde_json::json!({"new_key": "new_value"}), vec![]);
+    /// db.merge_and_write(tree).unwrap();
+    /// assert_eq!(db.get("key").unwrap().inner, serde_json::json!({"new_key": "new_value"}));
+    /// ```
+    pub fn merge_and_write(&mut self, tree: Tree) -> Result<(), NanoDBError> {
+        self.merge(tree)?;
+        self.write()
+    }
+
     /// Write the current state of the JSON data to disk asynchronously
     pub fn write(&mut self) -> Result<(), NanoDBError> {
         let data_guard = self
@@ -136,53 +219,5 @@ impl NanoDB {
 
     fn _read_lock(&mut self) -> Result<RwLockReadGuard<'_, Value>, NanoDBError> {
         self.data.read().map_err(|_| NanoDBError::RwLockReadError)
-    }
-
-    /// Pushes a value to a nested array specified by a string path.
-    /// The path is a slice of strings, representing a sequence of keys (and indices represented as strings for arrays).
-    pub fn merge<T, S>(&mut self, path: &[S], value: T) -> Result<(), NanoDBError>
-    where
-        T: Serialize,
-        S: Into<String> + Clone,
-    {
-        let mut data = self
-            .data
-            .write()
-            .map_err(|_| NanoDBError::RwLockWriteError)?;
-
-        // Navigate to the target array using the path.
-        let mut current = &mut *data;
-        for p in path {
-            let key = p.clone().into();
-
-            // Attempt to treat the current value as an object and navigate to the next key.
-            if current.is_object() {
-                current = current
-                    .get_mut(&key)
-                    .ok_or_else(|| anyhow!("Key not found: {}", key))?;
-            } else if let Ok(idx) = key.parse::<usize>() {
-                // If the current value is not an object, try to parse the key as an array index.
-                if !current.is_array() {
-                    return Err(NanoDBError::NotAnArray);
-                }
-                let arr = current.as_array_mut().ok_or(NanoDBError::NotAnArray)?;
-                current = arr.get_mut(idx).ok_or(NanoDBError::IndexOutOfBounds)?;
-            } else {
-                // If the key is not a valid index and we're not currently at an object, it's an error.
-                return Err(NanoDBError::InvalidJSONPath);
-            }
-        }
-
-        // Now `current` should be the target array.
-        if let Some(arr) = current.as_array_mut() {
-            dbg!(&arr);
-            let serialized_value = serde_json::to_value(value)?;
-            arr.push(serialized_value);
-            dbg!(&arr);
-            dbg!(&data);
-            Ok(())
-        } else {
-            Err(NanoDBError::NotAnArray)
-        }
     }
 }
