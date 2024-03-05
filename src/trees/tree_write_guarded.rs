df3f49bec26d653625eaf0961167bc6d037d84b1
2bonahill <tobias.ambuehl@gmail.com>
2024-03-05 13:43:25 +0100

    rsdoc

--- src/trees/tree_write_guarded.rs
+++ src/trees/tree_write_guarded.rs
@@ -23,37 +23,72 @@ impl<'a> WriteGuardedTree<'a> {
         }
     }
 
-    // Implement methods specific to WriteGuardedTree here
+    /// Retrieves the value associated with a given key in the JSON data of the TreeWriteGuarded instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to retrieve the value for.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(&mut Self)` - The TreeWriteGuarded instance itself after the retrieval. This allows for method chaining.
+    /// * `Err(NanoDBError::InvalidJSONPath)` - If the path to the key in the JSON data is invalid.
     pub fn get(&mut self, key: &str) -> Result<&mut Self, NanoDBError> {
         self.inner = self.inner.clone().get(key)?;
         Ok(self)
     }
 
+    /// Retrieves the value at a given index in the JSON array of the TreeWriteGuarded instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - The index to retrieve the value from.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(&mut Self)` - The TreeWriteGuarded instance itself after the retrieval. This allows for method chaining.
+    /// * `Err(NanoDBError::InvalidJSONPath)` - If the path to the index in the JSON data is invalid.
+    /// * `Err(NanoDBError::IndexOutOfBounds)` - If the index is out of bounds.
     pub fn at(&mut self, index: usize) -> Result<&mut Self, NanoDBError> {
         self.inner = self.inner.clone().at(index)?;
         Ok(self)
     }
 
+    /// Inserts a key-value pair into the inner JSON object of the TreeWriteGuarded instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to insert the value for.
+    /// * `value` - The value to insert. This value must implement the `Serialize` trait.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(&mut Self)` - The TreeWriteGuarded instance itself after the insertion. This allows for method chaining.
+    /// * `Err(NanoDBError::InvalidJSONPath)` - If the path to the key in the JSON data is invalid.
+    /// * `Err(NanoDBError::IndexOutOfBounds)` - If an array index in the path is out of bounds.
     pub fn insert<T: Serialize>(&mut self, key: &str, value: T) -> Result<&mut Self, NanoDBError> {
-        dbg!("hi from insert");
-        dbg!(&self.inner);
         self.inner = self.inner.insert(key, value)?;
-        dbg!(&self.inner);
 
-        // *self._guard = self.inner.inner();
         self.merge_from(self.inner.clone())?;
-
-        dbg!("hi back from merge_from");
-        dbg!(&self);
 
         Ok(self)
     }
 
+    /// Converts the inner JSON object of the TreeWriteGuarded instance into a specified type.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T` - The type to convert the JSON object into. This type must implement the `Deserialize` trait.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(T)` - The JSON object converted into the specified type.
+    /// * `Err(serde_json::Error)` - If there was an error during the conversion.
     pub fn into<T: for<'de> serde::Deserialize<'de>>(&mut self) -> Result<T, serde_json::Error> {
         serde_json::from_value(self.inner.inner())
     }
 
-    /// Merges the JSON data from another Tree instance into this Tree instance.
+    /// Merges the JSON data from another Tree instance into this guarded instance.
     ///
     /// # Arguments
     ///
