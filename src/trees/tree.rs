20346d49f5ca6ab8f7311f907feb8c3557f5c53a
2bonahill <tobias.ambuehl@gmail.com>
2024-03-07 19:16:53 +0100

    refactoring merge

--- src/trees/tree.rs
+++ src/trees/tree.rs
@@ -192,44 +192,6 @@ impl Tree {
         Ok(self.clone())
     }
 
-    /// Merges the JSON data from another Tree instance into this guarded instance.
-    ///
-    /// # Arguments
-    ///
-    /// * `other` - The other Tree instance to merge from.
-    fn merge(destination: &mut Tree, source: Tree) -> Result<&mut Self, NanoDBError> {
-        let path = destination.path();
-        let mut current = &mut destination.inner;
-
-        for p in path {
-            match p {
-                PathStep::Key(key) => {
-                    if current.is_object() {
-                        let obj = current.as_object_mut().unwrap();
-                        match obj.get_mut(&key) {
-                            Some(value) => current = value,
-                            None => return Err(NanoDBError::InvalidJSONPath),
-                        }
-                    } else {
-                        return Err(NanoDBError::InvalidJSONPath);
-                    }
-                }
-                PathStep::Index(idx) => {
-                    if current.is_array() {
-                        let arr = current.as_array_mut().unwrap();
-                        current = arr.get_mut(idx).ok_or(NanoDBError::IndexOutOfBounds(idx))?;
-                    } else {
-                        return Err(NanoDBError::InvalidJSONPath);
-                    }
-                }
-            }
-        }
-
-        *current = source.inner();
-
-        Ok(destination)
-    }
-
     /// Pushes a value to the tree if it's an array.
     ///
     /// # Arguments
